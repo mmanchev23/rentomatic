e91d9359711756101dcd508bc2d3afc5044dc151
mmanchev23 <mmanchev2003@gmail.com>
2022-03-16 13:09:10 +0200

    Added User model ...

--- car/models.py
+++ car/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pin = models.CharField(max_length=10, null=False, blank=False)
+    phone_number = models.CharField(max_length=10, null=False, blank=False)
+    pass
+
+    def __str__(self) -> str:
+        return self.username
--- rent/settings.py
+++ rent/settings.py
@@ -9,6 +9,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'car.User'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
