a95a83cb20ec426c01587de63d017e6e7b7f8cc6
mmanchev23 <mmanchev2003@gmail.com>
2022-03-16 12:48:36 +0200

    Configured routing ...

---
+++ car/urls.py
@@ -0,0 +1,14 @@
+
+from .views import *
+from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("admin/", admin.site.urls, name="admin"),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
