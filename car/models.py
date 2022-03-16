57bc40f590a7d624334fb05b8f1a8f6bc7ea9475
mmanchev23 <mmanchev2003@gmail.com>
2022-03-16 13:11:18 +0200

    Fixed a bug in the User model ...

--- car/models.py
+++ car/models.py
@@ -1,7 +1,6 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
 
 
 class User(AbstractUser):
