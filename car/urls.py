f75cb17bfb26d7a1acb88ac4b115508e70c51778
mmanchev23 <mmanchev2003@gmail.com>
2022-03-17 23:33:56 +0200

    Added log out functionality ...

--- car/urls.py
+++ car/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", index, name="index"),
+    path("logout/", logout_view, name="logout"),
     path("admin/", admin.site.urls, name="admin"),
 ]
 
--- car/views.py
+++ car/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+import os
+from .models import *
+from django.db.models import Q
+from django.urls import reverse
+from django.contrib import messages
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 
 
 def index(request):
     return render(request, "car/index.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
