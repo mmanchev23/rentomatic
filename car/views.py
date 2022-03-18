c99288b7474e5360ad966ea574b15d40deedb60e
mmanchev23 <mmanchev2003@gmail.com>
2022-03-18 12:55:34 +0200

    Added log in feature ...

--- car/views.py
+++ car/views.py
@@ -13,6 +13,19 @@ from django.contrib.auth import authenticate, login, logout
 def index(request):
     return render(request, "car/index.html")
 
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        messages.success(request, "You have logged in successfully!")
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        messages.error(request, "Invalid username and/or password!")
+        return render(request, "car/index.html")
+
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
